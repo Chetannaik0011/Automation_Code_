0ce1b4b824eddbb359d5d4428f72b7def85f1ec3
Chetannaik0011 <chetannaik044@gmail.com>
2024-05-07 22:02:52 +0530

    add

--- script/test_cases_for_login.py
+++ script/test_cases_for_login.py
@@ -2,6 +2,8 @@ import time
 
 from generic.basic_file import Base_Test
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as Ec
 
 
 
@@ -16,17 +18,17 @@ class Test_valid(Base_Test):
         input_email.send_keys("chetannaik044@gmail.com")
         input_password.send_keys("Chetan@11")
         sign_in.click()
-
-        time.sleep(5)
-        self.driver.get_screenshot_as_file("page.png")
-        expect='dashboard'
         try:
-            if expect in self.driver.current_url:
-                print("valid credential successfully login and navigate to dashboard")
-
-
+            wait=WebDriverWait(self.driver,10)
+            wait.until(Ec.url_to_be("https://app.leadzen.ai/dashboard"))
+            assert self.driver.current_url=="https://app.leadzen.ai/dashboard"
+            print("valid credential successfully login and navigate to dashboard")
         except:
-            print('login credential is invalid')
+            print("invalid credential")
+
+
+
+
 
 
 
